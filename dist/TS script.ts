1831a89ee7089b24fb3e7b12eb1187bef109dd96
Mazhorova <mazhorova1987@mail.ru>
2023-04-12 18:09:09 +0300

    first commit

---
+++ dist/TS script.ts
@@ -0,0 +1,14 @@
+// function sayError() {
+//   const x = 5;
+//   const y = 10;
+//   console.log(x + y)
+// }
+
+//  sayError()
+ 
+//  function unixTime() {
+//   let time = Math.floor(new Data().getTime())
+//   console.log(time)
+//  }
+//  unixTime()
+//  console.log('hello')
