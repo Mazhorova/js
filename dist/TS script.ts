d1156a61814d65e22a7c227a2549860da4407809
Mazhorova <mazhorova1987@mail.ru>
2023-05-31 22:43:51 +0300

    change

--- dist/TS script.ts
+++ dist/TS script.ts
@@ -12,3 +12,46 @@
 //  }
 //  unixTime()
 //  console.log('hello')
+let buttonPlus = document.getElementById('buttonPlus');
+let buttonMinus = document.getElementById('buttonMinus');
+let buttonMultiply = document.getElementById('buttonMultiply');
+let buttonDevide = document.getElementById('buttonDevide');
+
+
+function onbuttonPlusClick() {
+  let input1 = document.getElementById('number1');
+  let input2 = document.getElementById('number2');
+  let number1 = Number('input1.value');
+  let number2 = Number ('input2.value');
+  let summ = number1 + number2;
+  window.alert(summ);
+}
+function onbuttonMinusClick() {
+  let input1 = document.getElementById('number1');
+  let input2 = document.getElementById('number2');
+  let number1 = Number('input1.value');
+  let number2 = Number ('input2.value');
+  let summ = number1 - number2;
+  window.alert(summ);
+}
+
+function onbuttonMultiplyClick() {
+  let input1 = document.getElementById('number1');
+  let input2 = document.getElementById('number2');
+  let number1 = Number('input1.value');
+  let number2 = Number ('input2.value');
+  let summ = number1 * number2;
+  window.alert(summ);
+}
+function onbuttonDevideClick() {
+  let input1 = document.getElementById('number1');
+  let input2 = document.getElementById('number2');
+  let number1 = Number('input1.value');
+  let number2 = Number ('input2.value');
+  let summ = number1 / number2;
+  window.alert(summ);
+}
+buttonPlus.addEventListener('click',onbuttonPlusClick);
+buttonMinus.addEventListener('click',onbuttonPlusClick);
+buttonMultiply.addEventListener('click',onbuttonPlusClick);
+buttonDevide.addEventListener('click',onbuttonPlusClick);
--- dist/dist/TS script.js
+++ dist/dist/TS script.js
@@ -10,3 +10,43 @@
 //  }
 //  unixTime()
 //  console.log('hello')
+var buttonPlus = document.getElementById('buttonPlus');
+var buttonMinus = document.getElementById('buttonMinus');
+var buttonMultiply = document.getElementById('buttonMultiply');
+var buttonDevide = document.getElementById('buttonDevide');
+function onbuttonPlusClick() {
+    var input1 = document.getElementById('number1');
+    var input2 = document.getElementById('number2');
+    var number1 = Number('input1.value');
+    var number2 = Number('input2.value');
+    var summ = number1 + number2;
+    window.alert(summ);
+}
+function onbuttonMinusClick() {
+    var input1 = document.getElementById('number1');
+    var input2 = document.getElementById('number2');
+    var number1 = Number('input1.value');
+    var number2 = Number('input2.value');
+    var summ = number1 - number2;
+    window.alert(summ);
+}
+function onbuttonMultiplyClick() {
+    var input1 = document.getElementById('number1');
+    var input2 = document.getElementById('number2');
+    var number1 = Number('input1.value');
+    var number2 = Number('input2.value');
+    var summ = number1 * number2;
+    window.alert(summ);
+}
+function onbuttonDevideClick() {
+    var input1 = document.getElementById('number1');
+    var input2 = document.getElementById('number2');
+    var number1 = Number('input1.value');
+    var number2 = Number('input2.value');
+    var summ = number1 / number2;
+    window.alert(summ);
+}
+buttonPlus.addEventListener('click', onbuttonPlusClick);
+buttonMinus.addEventListener('click', onbuttonPlusClick);
+buttonMultiply.addEventListener('click', onbuttonPlusClick);
+buttonDevide.addEventListener('click', onbuttonPlusClick);
