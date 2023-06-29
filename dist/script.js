a7612321e16dcc7645566754bd08a03f10514397
Mazhorova <mazhorova1987@mail.ru>
2023-06-29 21:47:58 +0300

    change

--- dist/script.js
+++ dist/script.js
@@ -1,74 +1,22 @@
-// function sayError() {
-//   const x = 5;
-//   const y = 10;
-//   console.log(x + y)
-// }
-//  sayError()
-//  function unixTime() {
-//   let time = Math.floor(new Data().getTime())
-//   console.log(time)
-//  }
-//  unixTime()
-//  console.log('hello')
-// const user = {
-//   firsname: "Denis",
-//   age: 30,
-//   isAdmin: true,
-//   head: 3,
-//   email: 'testimoost.com',
-//   'user-address': {
-//     city:"Pekin"
-//   }
-// }
-// let value;
-// value = user.firsname;
-// value = user['head'];
-// value = user['user-address'];
-// value = user['user-address'].city;
-// user.firsname = 'Den'
-// value = user.firsname;
-// user['user-address'].city = 'Turin'
-// console.log(value);
-//  console.log(user);
-var buttonPlus = document.getElementById('buttonPlus');
-var buttonMinus = document.getElementById('buttonMinus');
-var buttonMultiply = document.getElementById('buttonMultiply');
-var buttonDevide = document.getElementById('buttonDevide');
-function onbuttonPlusClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-    var number1 = Number(input1 === null || input1 === void 0 ? void 0 : input1.value);
-    var number2 = Number(input2 === null || input2 === void 0 ? void 0 : input2.value);
-    var summ = number1 + number2;
-    window.alert(summ);
-}
-function onbuttonMinusClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-    var number1 = Number(input1 === null || input1 === void 0 ? void 0 : input1.value);
-    var number2 = Number(input2 === null || input2 === void 0 ? void 0 : input2.value);
-    var summ = number1 - number2;
-    window.alert(summ);
-}
-function onbuttonMultiplyClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-    var number1 = Number(input1 === null || input1 === void 0 ? void 0 : input1.value);
-    var number2 = Number(input2 === null || input2 === void 0 ? void 0 : input2.value);
-    var summ = number1 * number2;
-    window.alert(summ);
-}
-function onbuttonDevideClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-    var number1 = Number(input1 === null || input1 === void 0 ? void 0 : input1.value);
-    var number2 = Number(input2 === null || input2 === void 0 ? void 0 : input2.value);
-    var summ = number1 / number2;
-    window.alert(summ);
-}
-if (buttonPlus)
-    buttonPlus.addEventListener('click', onbuttonPlusClick);
-buttonPlus === null || buttonPlus === void 0 ? void 0 : buttonPlus.addEventListener('click', onbuttonPlusClick);
-buttonMinus === null || buttonMinus === void 0 ? void 0 : buttonMinus.addEventListener('click', onbuttonPlusClick);
-buttonMultiply === null || buttonMultiply === void 0 ? void 0 : buttonMultiply.addEventListener('click', onbuttonPlusClick);
-buttonDevide === null || buttonDevide === void 0 ? void 0 : buttonDevide.addEventListener('click', onbuttonPlusClick);
+var model = [
+    { type: 'tittle', value: 'hello js' },
+    { type: 'text', value: 'here is go' },
+    { type: 'columns', value: [
+            '111111',
+            '222222',
+            '333333',
+        ] }
+];
+var site = document.querySelector(selectors, '#site');
+model.forEach(function (block) {
+    var html = '';
+    if (block.type === 'tittle') {
+        html = "\n    <div class=\"row\">\n      <div class=\"col-sm\">\n        <h1>" + block.value + "</h1>\n      </div>\n    </div>";
+    }
+    else if (block.type === 'text') {
+        html = "\n    <div class=\"row\">\n      <div class=\"col-sm\">\n        <p>" + block.value + "</p>\n  </div>\n </div>";
+    }
+    else if (block.type === 'columns') {
+    }
+    site.insertAdjacentHTML(where, 'beforeend', html);
+});
--- script.ts
+++ script.ts
@@ -1,82 +1,34 @@
-// function sayError() {
-//   const x = 5;
-//   const y = 10;
-//   console.log(x + y)
-// }
+const model = [
+  {type: 'tittle', value: 'hello js'},
 
-//  sayError()
- 
-//  function unixTime() {
-//   let time = Math.floor(new Data().getTime())
-//   console.log(time)
-//  }
-//  unixTime()
-//  console.log('hello')
-// const user = {
-//   firsname: "Denis",
-//   age: 30,
-//   isAdmin: true,
-//   head: 3,
-//   email: 'testimoost.com',
-//   'user-address': {
-//     city:"Pekin"
+  {type: 'text', value: 'here is go'},
+  {type: 'columns', value: [
+ '111111',
+ '222222',
+ '333333',
+  ]}
+]
+const site = document.querySelector(selectors: '#site') 
+model.forEach(block => {
+  let html = ''
+  if (block.type === 'tittle') {
+    html = `
+    <div class="row">
+      <div class="col-sm">
+        <h1>${block.value}</h1>
+      </div>
+    </div>`
+  } else if (block.type === 'text') 
+  {
+    html = `
+    <div class="row">
+      <div class="col-sm">
+        <p>${block.value}</p>
+  </div>
+ </div>`
+  } else if (block.type === 'columns') {
 
-//   }
-// }
-// let value;
-// value = user.firsname;
-// value = user['head'];
-// value = user['user-address'];
-// value = user['user-address'].city;
-// user.firsname = 'Den'
-// value = user.firsname;
-// user['user-address'].city = 'Turin'
-// console.log(value);
-//  console.log(user);
-
-let buttonPlus = document.getElementById('buttonPlus');
-let buttonMinus = document.getElementById('buttonMinus');
-let buttonMultiply = document.getElementById('buttonMultiply');
-let buttonDevide = document.getElementById('buttonDevide');
-
-
-function onbuttonPlusClick() {
-  let input1 = document.getElementById('number1');
-  let input2 = document.getElementById('number2');
-  let number1 = Number(input1?.value);
-  let number2 = Number (input2?.value);
-  let summ = number1 + number2;
-  window.alert(summ);
-}
-function onbuttonMinusClick() {
-  let input1 = document.getElementById('number1');
-  let input2 = document.getElementById('number2');
-  let number1 = Number(input1?.value);
-  let number2 = Number (input2?.value);
-  let summ = number1 - number2;
-  window.alert(summ);
-}
-
-function onbuttonMultiplyClick() {
-  let input1 = document.getElementById('number1');
-  let input2 = document.getElementById('number2');
-  let number1 = Number(input1?.value);
-  let number2 = Number (input2?.value);
-  let summ = number1 * number2;
-  window.alert(summ);
-}
-function onbuttonDevideClick() {
-  let input1 = document.getElementById('number1');
-  let input2 = document.getElementById('number2');
-  let number1 = Number(input1?.value);
-  let number2 = Number (input2?.value);
-  let summ = number1 / number2;
-  window.alert(summ);
-}
-
-if (buttonPlus) buttonPlus.addEventListener('click',onbuttonPlusClick);
-
-buttonPlus?.addEventListener('click',onbuttonPlusClick);
-buttonMinus?.addEventListener('click',onbuttonPlusClick);
-buttonMultiply?.addEventListener('click',onbuttonPlusClick);
-buttonDevide?.addEventListener('click',onbuttonPlusClick);
+  }
+  
+  site.insertAdjacentHTML(where: 'beforeend',html) 
+})
