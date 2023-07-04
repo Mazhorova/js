ccaf1b004307ead3dee00e0691acec134ffd4495
Mazhorova <mazhorova1987@mail.ru>
2023-07-04 13:22:54 +0300

    bih

--- dist/script.js
+++ dist/script.js
@@ -1,22 +1,66 @@
-var model = [
-    { type: 'tittle', value: 'hello js' },
-    { type: 'text', value: 'here is go' },
-    { type: 'columns', value: [
-            '111111',
-            '222222',
-            '333333',
-        ] }
-];
-var site = document.querySelector(selectors, '#site');
-model.forEach(function (block) {
-    var html = '';
-    if (block.type === 'tittle') {
-        html = "\n    <div class=\"row\">\n      <div class=\"col-sm\">\n        <h1>" + block.value + "</h1>\n      </div>\n    </div>";
+// let admin,name;
+//  name = "Jonn";
+//  admin = name;
+//  alert ('admin')
+// let name = prompt('как зовут', "")
+// alert()
+// let a = +prompt("первое чило",10)
+// let b = +prompt("второе число",20)
+// alert(a+b)
+// let year = prompt ('в каком году')
+// if (year == 2015) {
+//   alert("вы молодец");
+// } else {
+//   alert("нет")
+// }
+// let year = prompt ('в какщм году') 
+//   if ( year < 2015 ) {
+//   alert('рано');
+// } else if ( year > 2019 ) {
+// alert('много'); }
+// else {
+//   alert('правильно');
+// }
+// let age = prompt ('возраст', 18);
+// let message = (age < 3 ) ? 'здраствуй малыш':
+// (age < 18) ? 'привет':
+// ( age < 90 ) ? 'здрасти':
+// alert( message );
+// let age = prompt('Возраст?', 18);
+// let message = (age < 3) ? 'Здравствуй, малыш!' :
+//   (age < 18) ? 'Привет!' :
+//   (age < 100) ? 'Здравствуйте!' :
+//   'Какой необычный возраст!';
+// alert( message );
+// let message = prompt ('как называется фирма', '');
+// if (message == 'ecma') {
+//   alert('верно');
+// } else {
+//   alert('не знаете')
+// }
+// let value = prompt ();
+// if (value > 0) {
+//   alert(1);
+// }  else if (value < 0) {
+//   alert(-1);
+// }
+// let result = (a + b < 4) ? 'мало' : 'много';
+// let message = prompt(login == 'Сотрудник') ? 'привет':
+// (login == 'директор') ? 'здрасти':
+// (login == '') ? 'нет имени':
+// alert(message)
+// if (!(age >= 14 && age <= 90)) 
+// alert(age)
+var surName = prompt("КТО ТАМ", '');
+if (surName === 'Админ') {
+    var pass = prompt('Пароль', '');
+    if (pass === 'Я главный') {
+        alert('здравствуйте');
     }
-    else if (block.type === 'text') {
-        html = "\n    <div class=\"row\">\n      <div class=\"col-sm\">\n        <p>" + block.value + "</p>\n  </div>\n </div>";
+    else if (pass === '' || pass === null) {
+        alert('отмена');
     }
-    else if (block.type === 'columns') {
+    else {
+        alert('неправильно');
     }
-    site.insertAdjacentHTML(where, 'beforeend', html);
-});
+}
--- script.ts
+++ script.ts
@@ -1,34 +1,74 @@
-const model = [
-  {type: 'tittle', value: 'hello js'},
+// let admin,name;
+//  name = "Jonn";
+//  admin = name;
+//  alert ('admin')
 
-  {type: 'text', value: 'here is go'},
-  {type: 'columns', value: [
- '111111',
- '222222',
- '333333',
-  ]}
-]
-const site = document.querySelector(selectors: '#site') 
-model.forEach(block => {
-  let html = ''
-  if (block.type === 'tittle') {
-    html = `
-    <div class="row">
-      <div class="col-sm">
-        <h1>${block.value}</h1>
-      </div>
-    </div>`
-  } else if (block.type === 'text') 
-  {
-    html = `
-    <div class="row">
-      <div class="col-sm">
-        <p>${block.value}</p>
-  </div>
- </div>`
-  } else if (block.type === 'columns') {
+// let name = prompt('как зовут', "")
+// alert()
 
+// let a = +prompt("первое чило",10)
+// let b = +prompt("второе число",20)
+// alert(a+b)
+// let year = prompt ('в каком году')
+// if (year == 2015) {
+//   alert("вы молодец");
+// } else {
+//   alert("нет")
+// }
+
+// let year = prompt ('в какщм году') 
+//   if ( year < 2015 ) {
+//   alert('рано');
+// } else if ( year > 2019 ) {
+// alert('много'); }
+// else {
+//   alert('правильно');
+// }
+// let age = prompt ('возраст', 18);
+// let message = (age < 3 ) ? 'здраствуй малыш':
+// (age < 18) ? 'привет':
+// ( age < 90 ) ? 'здрасти':
+// alert( message );
+// let age = prompt('Возраст?', 18);
+
+// let message = (age < 3) ? 'Здравствуй, малыш!' :
+//   (age < 18) ? 'Привет!' :
+//   (age < 100) ? 'Здравствуйте!' :
+//   'Какой необычный возраст!';
+
+// alert( message );
+
+// let message = prompt ('как называется фирма', '');
+// if (message == 'ecma') {
+//   alert('верно');
+// } else {
+//   alert('не знаете')
+// }
+// let value = prompt ();
+// if (value > 0) {
+//   alert(1);
+// }  else if (value < 0) {
+//   alert(-1);
+// }
+
+// let result = (a + b < 4) ? 'мало' : 'много';
+
+// let message = prompt(login == 'Сотрудник') ? 'привет':
+// (login == 'директор') ? 'здрасти':
+// (login == '') ? 'нет имени':
+// alert(message)
+
+// if (!(age >= 14 && age <= 90)) 
+// alert(age)
+
+let surName = prompt ("КТО ТАМ", '');
+if (surName === 'Админ') {
+  let pass = prompt('Пароль', '');
+  if (pass === 'Я главный') {
+    alert('здравствуйте');
+  } else if (pass === '' || pass === null) {
+    alert('отмена')
+  } else {
+    alert('неправильно')
   }
-  
-  site.insertAdjacentHTML(where: 'beforeend',html) 
-})
+}
